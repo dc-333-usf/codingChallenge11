9747577e5c73abfd2206b1140331220535b71057
Drew Cook <dc333@usf.edu>
2025-03-01 12:50:53 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -13,8 +13,8 @@ class Book { //create the new class
     }
 
     updateCopies(quantity) { //create a function to update the variable "this.copies"
-        if (typeof quantity !== `number`) { //first we check if the input is a number
-            console.log(`Invalid input. Please input a number.`); //if it's not, we return an error message
+        if (typeof quantity !== "number") { //first we check if the input is a number
+            return console.log(`Invalid input. Please input a number.`); //if it's not, we return an error message
         }
         if (quantity < 0) { //then, if it passes the first if condition, check for a negative value
             if (Math.abs(quantity) > this.copies) { //if the absolute value of the quantity is more than the number of copies
@@ -31,4 +31,32 @@ class Book { //create the new class
 const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
 console.log(book1.getDetails());
 book1.updateCopies(-1);
-console.log(book1.getDetails());
+console.log(book1.getDetails()); //test data
+
+//Task 2: Creating a borrower class. Library member scenario.
+class Borrower { //create the borrower class
+    constructor(name, borrowerId) { //allow something with a name and borrower ID to be an instance of the class
+        this.name = name;
+        this.borrowerId = borrowerId;
+        this.borrowedBooks = []; //create an empty array to store the books they borrow
+    }
+
+    borrowBook(book) { //create a method inside the class
+            this.borrowedBooks.push(book); //push the specified book to the array
+    }
+
+    returnBook(book) { //create another method to take books out of the array, meaning they are no longer borrowed
+        const index = this.borrowedBooks.indexOf(book); //find the index in the array that contains the specified book
+        if (index !== -1) { //if the index is not equal to negative one, in other words, if the instance is found
+            this.borrowedBooks.splice(index, 1); //splice the array at the index of the book we want to remove, remove one item 
+        } else { //if the index of the book is -1, meaning it was not found
+            return console.log(`Book not found.`) //return an error message
+        }
+    }
+}
+
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks); //test data
